10bffcdba9f98a378705678f9350cdf0fd37657e
Jeremy <jpoff1@vols.utk.edu>
2017-01-12 17:40:00 -0500

    added matrix
    added matrix

--- src/hw1/go.py
+++ src/hw1/go.py
@@ -1,17 +1,20 @@
 import sys
 from graph import Graph
 from graph import make
-
+from graph import GraphException
+from graph import Matrix
 
 def go():
     if ( len(sys.argv) == 2 ):
         filename = str(sys.argv[1])
         graph = make( filename )
-        
+
         print ("Max Degree: " + str(graph.degree("max")))
         print ("Min Degree: " + str(graph.degree("min")))
         print ("Density: " + str(graph.density()))
     else:
-        print ("Must supply a valid graph file")
+        print (GraphException("You must supply a valid graph file"))
+
+
 
 go()
--- src/hw1/graph.py
+++ src/hw1/graph.py
@@ -12,8 +12,8 @@ def make(filename):
 
         # strip the wack n if present
         for i in values:
-            i = int(str(i).strip("\n"))    
-        
+            i = int(str(i).strip("\n"))
+
         # if first get graph verts n edges
         if linenum == 0:
             verts = values[0]
@@ -26,6 +26,36 @@ def make(filename):
         linenum += 1
     return graph
 
+class GraphException(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+### not used, just messing with python overloading
+class Matrix:
+    def __init__(self, r, c):
+        self.rows = r
+        self.cols = c
+        self.data = [[ 0 for x in range(self.cols)] for y in range(self.rows)]
+    def __getitem__(self, key):
+        print ("key: " + str(key))
+        return self.data[key]
+    def __setitem__(self, key, value):
+        print ("set key: " + str(key) + " val: " + str(value))
+        self.data[key] = value
+    def output(self):
+        for  i in range(self.rows):
+            row = ""
+            for j in range (self.cols):
+                row += (str(self.data[i][j]) + "   ")
+            print ( row + "\n")
+    def set(self, a, b, val):
+        self.data[a][b] = val
+    def fill(self, value):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.set(i,j,value)
 
 class Graph:
     def __init__(self, vs, es):
@@ -33,12 +63,16 @@ class Graph:
         self.edges = es
         self.data = [[ 0 for x in range(self.verts)] for y in range(self.verts)]
 
+    def __getitem__(self, key):
+        return self.data[key]
+
     def output(self):
         for  i in range(self.verts):
             row = ""
             for j in range (self.verts):
                 row += (str(self.data[i][j]) + "   ")
             print ( row + "\n")
+
     def connect(self, a, b):
         self.data[a][b] = 1
         self.data[b][a] = 1
@@ -63,7 +97,9 @@ class Graph:
             if (switch == "max"):
                 if (tmp > target):
                     target = tmp
-            else:
+            elif(switch == "min"):
                 if ( tmp < target):
                     target = tmp
+            else:
+                print (GraphException("Invalid switch passed to degree."))
         return target
