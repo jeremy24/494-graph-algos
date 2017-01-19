a2daf1f75b9f5a836ab642ecc1adb9df78fd9929
Jeremy <jpoff1@vols.utk.edu>
2017-01-19 10:38:39 -0500

    added hw 2

---
+++ src/hw2/go.py
@@ -0,0 +1,25 @@
+import sys
+from graph import Graph
+from graph import make
+from graph import GraphException
+from graph import Matrix
+
+def go():
+
+    if ( len(sys.argv) == 2 ):
+        filename = str(sys.argv[1])
+        graph = make( filename )
+
+        graph.dfs(0, None)
+
+        # print ("Max Degree: " + str(graph.degree("max")))
+        # print ("Min Degree: " + str(graph.degree("min")))
+        # print ("Density: " + str(graph.density()))
+        graph.output()
+    else:
+        print (GraphException("You must supply a valid graph file"))
+
+
+
+
+go()
