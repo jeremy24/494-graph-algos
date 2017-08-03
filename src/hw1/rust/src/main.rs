b749fb74efbaf074d591737bcf13dbc7edaa65bb
Jeremy <jpoff1@vols.utk.edu>
2017-08-03 16:26:38 -0400

    rust stuff:

--- src/hw1/rust/src/main.rs
+++ src/hw1/rust/src/main.rs
@@ -4,9 +4,9 @@ use std::env;
 use std::fs::File;
 use std::process;
 use std::io::prelude::*;
+use std::fmt;
 
-
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 struct Edge {
     x: u64,
     y: u64
@@ -29,11 +29,71 @@ impl Edge {
             }
         }
     }
+
 }
 
+
+impl fmt::Display for Edge {
+    fn fmt(&self, f: &mut fmt::Formatter) ->
+    fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+#[derive(Debug)]
 struct Header {
     num_vertices: u64,
     num_edges: u64
+}
+
+
+
+impl Header {
+    pub fn inc_edges(&mut self) { self.num_edges += 1; }
+    pub fn dec_edges(&mut self) { self.num_edges -= 1; }
+}
+
+impl fmt::Display for Header {
+    fn fmt(&self, f: &mut fmt::Formatter) ->
+    fmt::Result {
+        write!(f, "Num Vertices: {}  Num Edges: {}", self.num_vertices, self.num_edges)
+    }
+}
+
+
+struct Graph {
+    header: Header,
+    edges: Vec<Edge>
+}
+
+impl fmt::Display for Graph {
+    fn fmt(&self, f: &mut fmt::Formatter) ->
+    fmt::Result {
+        write!(f, "{}\n{:?}", self.header, self.edges)
+    }
+}
+
+
+impl Display for NumVec {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        write!(f, "{}\n{:?}", self.header);
+        write!(f, "{}", comma_separated)
+    }
+}
+
+
+
+impl Graph {
+    pub fn connect(&mut self, a: u64, b: u64) {
+        let edge = Edge::new(a, b);
+        match self.edges.binary_search(&edge) {
+            Ok(_) => println!("{} already in graph", edge),
+            Err(pos) => {
+                self.edges.insert(pos, edge);
+                self.header.inc_edges();
+            }
+        }
+    }
 }
 
 
@@ -84,13 +144,18 @@ fn main() {
         process::exit(2);
     }
     let header = Header{ num_vertices: num_verts, num_edges: num_edges };
+    let mut graph = Graph { header: header, edges: Vec::<Edge>::new() };
+
+    while i < data.len() {
+        graph.connect(data[i], data[i+1]);
+        i += 2;
+    }
+
+    println!("Graph: {}", graph);
 
     let mut outfile: String = filename.to_owned();
     outfile.push_str(".graph");
 
-
-    println!("{:?}", x);
-    println!("{:?}", y);
 
     println!("Outfile: {}", outfile);
     let fout = File::create(outfile).expect("Cannot open output file");
